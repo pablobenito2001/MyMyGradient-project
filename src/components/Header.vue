f0fd9f4ccea543492f3794c64431d8d369594b45
Plagga <benitopab@gmail.com>
2023-02-01 20:06:35 -0300

    Header section, 1-02/23

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+</script>
+
+<template>
+    <header class="Header">
+        <h1 class="Header-title">MyMyGradient</h1>
+        <span class="Header-name">By Pablo Benito</span>
+    </header>
+</template>
+
+<style scoped lang="scss">
+    .Header{
+        min-width: 100%;
+        padding: var(--gap);
+        background-color: var(--black);
+        &-title{
+            font-weight: bold;
+            font-size: 1.4em;
+            color: white;
+            display: inline-block;
+        }
+        &-name{
+            font-size: 0.6em;
+            color: white;
+        }
+    }
+</style>
